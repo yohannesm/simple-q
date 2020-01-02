5cdf697b56152d375b9f96661590d7fb1627d6d1
yohannesm <yohannes.marcell@gmail.com>
2020-01-02 14:15:35 -0800

    added simple implementation

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,4 +2,9 @@
 object Main extends App {
 
   println("Hello world!")
+  var q: Queue[Int] = new QueueImpl[Int](Queue.empty)
+  val q1 = q.enQueue(1)
+  val q2 = q1.enQueue(2)
+  val q3 = q2.enQueue(3)
+  q3.printQ
 }
--- src/main/scala/Queue.scala
+++ src/main/scala/Queue.scala
@@ -1,21 +1,42 @@
+import scala.collection.immutable
+import scala.collection.immutable._
+
 sealed trait Queue[T] {
   def isEmpty: Boolean
   def enQueue(t: T): Queue[T]
    // # Removes the element at the beginning of the immutable queue, and returns the new queue.
   def deQueue(): Queue[T]
   def head: Option[T]
+  def printQ: Unit
 }
 
-class QueueImpl[T] extends Queue[T] {
-  override def isEmpty: Boolean = ???
+class QueueImpl[T](private val lst: List[T]) extends Queue[T] {
+  //val lst: List[T] = scala.collection.immutable.List[T]()
+  //var lst: List[T] = scala.collection.immutable.List[T]()
 
-  override def enQueue(t: T): Queue[T] = ???
+  override def isEmpty: Boolean = lst.isEmpty
 
-  override def deQueue(): Queue[T] = ???
+  override def enQueue(t: T): Queue[T] = {
+    new QueueImpl[T](t :: lst)
+  }
 
-  override def head: Option[T] = ???
+  override def deQueue(): Queue[T] = {
+    new QueueImpl[T](lst.dropRight(1))
+  }
+
+  override def head: Option[T] = {
+    lst.headOption
+  }
+
+  override def printQ(): Unit = {
+    print("Queue is: ")
+    lst.foreach(x => print(s"$x ") )
+    println(lst.isEmpty)
+  }
 }
 
 object Queue {
-  def empty[T]: Queue[T] = ???
+  type Queue[T] = scala.collection.immutable.List[T]
+  //def Queue[T](xs: T*) = List(xs: _*)
+  def empty[T]: Queue[T] = List.empty[T]
 }
